c8e8bae5b521674936aec9ff7d16bd1804ea95b7
vivignutz <vgnutzmann@gmail.com>
2024-02-25 16:16:01 +0100

    authRouter and Middleware refactored

--- middlewares/auth.Middleware.js
+++ middlewares/auth.Middleware.js
@@ -1,30 +1,20 @@
 // auth.Middleware.js
 
 import jwt from "jsonwebtoken";
-import { User } from "../models/UserModel.js";
 
 const authMiddleware = async (req, res, next) => {
   try {
     const token = req.header("x-auth-token");
-    if (!token)
-      return res
-        .status(401)
-        .json({ msg: "No auth token, access denied" });
+    if (!token) {
+      return res.status(401).json({ msg: "No authentication token, authorization denied" });
+    }
 
     const verified = jwt.verify(token, "secretKey");
-    if (!verified)
-      return res
-        .status(401)
-        .json({ msg: "Token verification failed, authorization denied" });
+    if (!verified) {
+      return res.status(401).json({ msg: "Token verification failed, authorization denied" });
+    }
 
-    const user = await User.findById(verified.id);
-    if (!user)
-      return res
-        .status(401)
-        .json({ msg: "No user found with this token, authorization denied" });
-
-    // since the token was made out of the document id 
-    req.user = user.id;
+    req.user = verified.id;
     next();
   } catch (err) {
     res.status(500).json({ error: err.message });
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,10 +4,11 @@ import express from "express";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { User } from "../models/UserModel.js"; 
-//import router from "../controllers/authController.js";
 import authMiddleware from "../middlewares/auth.Middleware.js";
+import multer from "multer";
 
 const authRouter = express.Router();
+const upload = multer({ dest: "uploads/" });
 
 // Login Route
 authRouter.post("/login", async (req, res) => {
@@ -37,33 +38,26 @@ authRouter.post("/login", async (req, res) => {
       json({ token, user: { id: user._id, username: user.username } });
   } catch (err) {
     res
-      .status(500)
-      .json({ error: err.message });
+      .status(500).json({ error: err.message });
   }
 });
 
 // Signup Route
-authRouter.post("/signup", async (req, res) => {
+authRouter.post("/signup", upload.single("avatar"), async (req, res) => {
   try {
-    const { firstName, lastName, userName, email, password, confirmPassword, avatar } = req.body;
-    if ( !firstName || !lastName || !userName || !email || !password  || !confirmPassword || !avatar ) {
-      return res
-        .status(400)
-        .json({ msg: "Please fill in all required fields" });
+    const { firstName, lastName, userName, dateOfBirth, email, address, number, city, country, postalCode, password, confirmPassword, avatar } = req.body;
+    if ( !firstName || !lastName || !userName || !dateOfBirth || !address || !number  || !city || !country || !postalCode || !email || !password  || !confirmPassword || !avatar ) {
+      return res.status(400).json({ msg: "Please fill in all required fields" });
     }
     if (password.length < 6) {
-      return res
-        .status(400)
-        .json({ msg: "Password must be at least 6 characters" });
+      return res.status(400).json({ msg: "Password must be at least 6 characters" });
     }
     if (confirmPassword !== password) {
-      return res.status(400).json({ msg: "Meh... Passwords do not match :-( " });
+      return res.status(400).json({ msg: "Passwords do not match" });
     }
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res
-        .status(400)
-        .json({ msg: "A user with the same email already exists" });
+      return res.status(400).json({ msg: "A user with the same email already exists" });
     }
     const hashedPassword = await bcryptjs.hash(password, 8);
     const newUser = new User({ 
@@ -71,6 +65,12 @@ authRouter.post("/signup", async (req, res) => {
       lastName,
       userName, 
       email, 
+      dateOfBirth,
+      address,
+      number,
+      city,
+      country,
+      postalCode,
       password: hashedPassword,
       avatar,
     });
@@ -87,23 +87,25 @@ authRouter.post("/signup", async (req, res) => {
 authRouter.post("/tokenIsValid", authMiddleware, async (req, res) => {
   try {
     const token = req.header("x-auth-token");
-    if (!token) 
-      return res
-        .json(false);
+    if (!token) return res.json(false);
     const verified = jwt.verify(token, "secretKey");
-    if (!verified) 
-      return res
-        .json(false);
+    if (!verified) return res.json(false);
     const user = await User.findById(verified.id);
-    if (!user) 
-      return res
-        .json(false);
-    return res
-      .json(true);
+    if (!user) return res.json(false);
+    return res.json(true);
   } catch (err) {
-    res
-      .status(500)
-      .json({ error: err.message });
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Get user details route
+authRouter.get("/user", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user);
+    if (!user) return res.status(400).json({ msg: "User not found" });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
